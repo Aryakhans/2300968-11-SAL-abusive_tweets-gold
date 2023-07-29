db7d8cc09c5e369dd36c9a1f11339fc501dc16e5
aryakhans <salmanaryakhan17@gmail.com>
2023-07-29 20:07:09 +0700

    first commit

---
+++ model-deploy-learn/binar fd terakhir.py
@@ -0,0 +1,7 @@
+from google.colab import drive
+drive.mount('/gdrive')
+%cd /gdrive
+
+import pandas as pd #data manipulation
+import seaborn as sns #data visualization
+import matplotlib.pyplot as plt #data visualization
